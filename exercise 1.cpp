6f495a7ac9fa171eff82dc77bd8385414228f951
imran-dev-code <imrufighter@gmail.com>
2025-07-19 03:21:51 -0700

    Add files via upload

--- exercise 1.cpp
+++ exercise 1.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
-int main(){
-	cout<<"hello!";
+int main(){int x,y,sum;
+	cout<<"hello!"<<endl;
+	cout<<"enter first number:"<<endl;
+	cin>>x;
+	cout<<"enter second number:"<<endl;
+	cin>>y;
+	sum = x + y;
+	cout<<"sum:"<<sum<<endl;
+	cout<<"todays date: 19, jul, sat"<<endl;
+	return 0;
 }
