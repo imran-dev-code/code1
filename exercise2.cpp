9c10a17224635e9d4e20456b281b8b0c1fafc1e6
imran-dev-code <imrufighter@gmail.com>
2025-07-19 04:01:29 -0700

    Add files via upload

---
+++ exercise2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int age = 10;
+	cout<<"enter your age:";
+	cin>>age;
+	if (age > 7) {cout<<"you can use computer"<<endl;
+	} else {cout<<"you cant usse computer"<<endl;
+	}
+}
