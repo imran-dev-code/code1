ecb7eee28ec30f38de2d3fba6e39bce7b5ef9cce
imran-dev-code <imrufighter@gmail.com>
2025-07-19 03:35:04 -0700

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int age = 15;
+	float height = 1.55;
+	string name = "imran ";
+	char alphabet = 'A';
+	cout<<name<<"is "<<age<<" year old boy and "<<height<<"meter tall";
+}
